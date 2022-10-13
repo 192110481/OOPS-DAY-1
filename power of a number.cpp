b35fbbb996e8a0b8b4cfd6c46b5e9413c7fd6f0c
Venkatesh <112744029+192110481@users.noreply.github.com>
2022-10-13 11:39:28 +0530

    Add files via upload

---
+++ power of a number.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double x = 23, y = 4;
+	double result = pow(x, y);
+	cout << fixed << setprecision(2) << result << endl;
+	return 0;
+}
